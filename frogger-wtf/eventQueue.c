c7cd583e3e6af7b0c1bd56a472c21ffc522f47d7
nico <nico@nicoUbuntu>
2018-06-30 16:18:24 -0300

    cambio eventqueue

--- frogger-wtf/eventQueue.c
+++ frogger-wtf/eventQueue.c
@@ -150,7 +150,7 @@ bool emit_event( event_queue_t* queue , uint16_t type )
       (queue -> p2write) ++; // si no avanza una posicion
     }
     temp -> p2NextEv = (struct EVENT*) queue -> p2write;// guardo en el evento anterior un puntero al evento actual para no tener que problems con el movimiento en get_event
-    temp -> p2write -> ack = false;   // marco el lugar como ocupado
+    temp -> ack = false;   // marco el lugar como ocupado
     return true;
   }
 }
